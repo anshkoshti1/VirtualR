2a19f855e508850607fdb11edd14207daed39266
anshkoshti1 <adkbro4@gmail.com>
2024-09-15 09:21:27 +0530

    Landing page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import FeatureSection from "./components/FeatureSection"
+import Footer from "./components/Footer"
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
+import WorkFlow from "./components/WorkFlow"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
